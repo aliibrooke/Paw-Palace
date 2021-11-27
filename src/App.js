8de1fbc605c86ca32b9b74840507abed6c98cf50
aliibrooke <87044077+aliibrooke@users.noreply.github.com>
2021-11-27 17:01:14 -0500

    Inital Set-up/ Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              <Navbar light color="warning">
+              <div className="container">
+                  <NavbarBrand href="/">Paw Palace</NavbarBrand>
+              </div>
+              </Navbar>
+              <Directory />
+          </div>
+      );
+  }
 }
 
 export default App;
